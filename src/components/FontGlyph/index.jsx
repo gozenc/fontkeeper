06a948c5485a8fe281640c2fd981f01b1ea6931d
Fatih <fatihgozenc@gmail.com>
2023-06-24 09:41:40 +0200

    added analytics

--- src/components/FontGlyph/index.jsx
+++ src/components/FontGlyph/index.jsx
@@ -1,18 +1,22 @@
-import styles from "./font-glyph.module.scss"
+import styles from "./font-glyph.module.scss";
 
 export default function FontGlyph(props) {
-    const character = String.fromCharCode(props.data.unicode)
-    return (
-        <FontGlyphWrapper unicode={props.data.unicode} character={character} name={props.data.name} />
-    )
+  const character = String.fromCharCode(props.data.unicode);
+  return (
+    <FontGlyphWrapper
+      unicode={props.data.unicode}
+      character={character}
+      name={props.data.name}
+    />
+  );
 }
 
 export function FontGlyphWrapper(props) {
-    return (
-        <div className={`${styles.glyph} d-flex align-center justify-center`}>
-            <span className={styles.unicode}>{props.unicode}</span>
-            <span className={styles.char}>{props.character}</span>
-            <span className={styles.name}>{props.name}</span>
-        </div>
-    )
+  return (
+    <div className={`${styles.glyph} d-flex align-center justify-center`}>
+      <span className={styles.unicode}>{props.unicode}</span>
+      <span className={styles.char}>{props.character}</span>
+      <span className={styles.name}>{props.name}</span>
+    </div>
+  );
 }
